33de73b32b01ccb6fb1d1f9f1dd3154299cb4506
JaysonGangone <129904212+JaysonGangone@users.noreply.github.com>
2023-06-06 14:21:36 -0500

    Pokedex creation
    Removed all the old code and created a new pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert ('Hello World');
-let favoriteFood = 'pasta';
-document.write (favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['Grass', 'Poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['Fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['Water']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: ['electric']
+    }
+]
